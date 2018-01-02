4bc5444f891e89fb7bef75ea367ba436d01c77aa
DSMdongly <gdh0608@naver.com>
2018-01-02 23:25:59 +0900

    fixed test page

--- app/static/js/test/index.js
+++ app/static/js/test/index.js
@@ -4,3 +4,22 @@ socket.onmessage = (event) => {
     console.log(event.data);
 };
 
+document.getElementById("add-param-btn").onclick = (event) => {
+    let keyInput = document.createElement("input");
+    keyInput.type = "text";
+    keyInput.placeholder = "Key";
+
+    let valueInput = document.createElement("input");
+    valueInput.type = "text";
+    valueInput.placeholder = "Value";
+
+    let keyValueContainer = document.createElement("div");
+    keyValueContainer.classList.add("key-value-container");
+    keyValueContainer.style.display = "block";
+
+    keyValueContainer.appendChild(keyInput);
+    keyValueContainer.appendChild(valueInput);
+
+    let paramForm = document.createElement("key-value-form");
+    
+};
