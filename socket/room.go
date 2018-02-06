06fca90f48ca6559083582b0524458dc8ac09f12
DSMdongly <gdh0608@naver.com>
2018-02-06 16:31:18 +0900

    fixed typo

--- socket/room.go
+++ socket/room.go
@@ -40,7 +40,7 @@ func (rom *Room) Quit(cli *Client) {
 func (rom *Room) BroadCast(cli *Client, msg Message) {
 	for mid, mem := range rom.Clients {
 		if cli.Data["id"].(string) != mid {
-			cli.Output <- msg
+			mem.Output <- msg
 		}
 	}
 }
