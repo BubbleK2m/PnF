5bcff5e3415885254ad5d87d8a261a75e7d0d18e
DSMdongly <gdh0608@naver.com>
2018-03-14 19:33:27 +0900

    fixed config and init app

--- config/init.go
+++ config/init.go
@@ -1,31 +1,14 @@
 package config
 
 import (
-	"fmt"
-	"net"
-	"net/url"
 	"os"
 )
 
 var (
-	HTTP     map[string]string
-	JWT      map[string]string
-	Postgres map[string]string
+	HTTP map[string]string
 )
 
 func Init() {
 	HTTP = make(map[string]string)
 	HTTP["PORT"] = os.Getenv("PORT")
-
-	JWT = make(map[string]string)
-	JWT["SECRET"] = "55A95EAA446C2D545BC57A7F3BBAB"
-
-	pth, _ := url.Parse(os.Getenv("DATABASE_URL"))
-
-	Postgres = make(map[string]string)
-	Postgres["HOST"], _, _ = net.SplitHostPort(pth.Host)
-	Postgres["USER"] = pth.User.Username()
-	Postgres["PASSWORD"], _ = pth.User.Password()
-	Postgres["DB"] = pth.Path[1:]
-	Postgres["PATH"] = fmt.Sprintf("host=%s user=%s dbname=%s sslmode=disable password=%s", Postgres["HOST"], Postgres["USER"], Postgres["DB"], Postgres["PASSWORD"])
 }
--- main.go
+++ main.go
@@ -1,10 +1,7 @@
 package main
 
 import (
-	"log"
-
 	"github.com/DSMdongly/pnf/app"
-	"github.com/DSMdongly/pnf/app/model"
 	"github.com/DSMdongly/pnf/app/route"
 	"github.com/DSMdongly/pnf/config"
 	"github.com/DSMdongly/pnf/socket"
@@ -13,10 +10,6 @@ import (
 func main() {
 	config.Init()
 	socket.Init()
-
-	if err := model.Init(); err != nil {
-		log.Fatal(err)
-	}
 
 	app.Init()
 	route.Init()
