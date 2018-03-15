bcafcc8f90e64bb5d0e7755255d7010db8c82485
DSMdongly <gdh0608@naver.com>
2018-03-15 14:32:22 +0900

    added simple echo logic

--- app/route/handler/socket.go
+++ app/route/handler/socket.go
@@ -1,8 +1,6 @@
 package handler
 
 import (
-	"github.com/DSMdongly/pnf/socket"
-
 	"github.com/labstack/echo"
 
 	"golang.org/x/net/websocket"
@@ -11,10 +9,17 @@ import (
 func Socket() echo.HandlerFunc {
 	return func(ctx echo.Context) error {
 		websocket.Handler(func(con *websocket.Conn) {
-			cli := socket.NewClient(con)
-			defer cli.Close()
+			defer con.Close()
 
-			cli.Handle()
+			txt := ""
+
+			if err := websocket.Message.Receive(con, &txt); err != nil {
+				ctx.Logger().Error(err)
+			}
+
+			if err := websocket.Message.Send(con, &txt); err != nil {
+				ctx.Logger().Error(err)
+			}
 		}).ServeHTTP(ctx.Response(), ctx.Request())
 
 		return nil
